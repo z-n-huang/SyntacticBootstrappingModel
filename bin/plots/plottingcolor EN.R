7f5f65b92816956e45689538bd062e076c239e97
znhuang <znhuang@terpmail.umd.edu>
2020-05-26 10:54:48 -0400

    revised scripts

--- bin/plots/plottingcolor EN.R
+++ bin/plots/plottingcolor EN.R
@@ -50,39 +50,40 @@ verbreps_kl_results <- rbind(
 )
 
 # Results for model with a either-or JSD bias
-v2 <- read_csv("C:/Users/znhua/Documents/raspberry transfers/results 5-15 1e0 x10 en may19/verbreps_resultsen 5-15 1e0 2.csv")
-v3 <- read_csv("C:/Users/znhua/Documents/raspberry transfers/results 5-15 1e0 x10 en may19/verbreps_resultsen 5-15 1e0 3.csv")
-v4 <- read_csv("C:/Users/znhua/Documents/raspberry transfers/results 5-15 1e0 x10 en may19/verbreps_resultsen 5-15 1e0 4.csv")
-v5 <- read_csv("C:/Users/znhua/Documents/raspberry transfers/results 5-15 1e0 x10 en may19/verbreps_resultsen 5-15 1e0 5.csv")
+v2 <- read_csv("C:/Users/znhua/Documents/raspberry transfers/results 5-15 1e0 x10en may19/verbreps_resultsen 5-15 1e0 2.csv")
+v3 <- read_csv("C:/Users/znhua/Documents/raspberry transfers/results 5-15 1e0 x10en may19/verbreps_resultsen 5-15 1e0 3.csv")
+v4 <- read_csv("C:/Users/znhua/Documents/raspberry transfers/results 5-15 1e0 x10en may19/verbreps_resultsen 5-15 1e0 4.csv")
+v5 <- read_csv("C:/Users/znhua/Documents/raspberry transfers/results 5-15 1e0 x10en may19/verbreps_resultsen 5-15 1e0 5.csv")
 v2$itr <- ifelse(v2$itr == 0, 2, 3)
 v3$itr <- ifelse(v3$itr == 0, 4, 5)
 v4$itr <- ifelse(v4$itr == 0, 6, 7)
 v5$itr <- ifelse(v5$itr == 0, 8, 9)
 
 verbreps_results <- rbind(
-  read_csv("C:/Users/znhua/Documents/raspberry transfers/results 5-15 1e0 x10 en may19/verbreps_resultsen 5-15 1e0 1.csv"),
+  read_csv("C:/Users/znhua/Documents/raspberry transfers/results 5-15 1e0 x10en may19/verbreps_resultsen 5-15 1e0 1.csv"),
   v2, v3, v4, v5
 )
 
-p2 <- read_csv("C:/Users/znhua/Documents/raspberry transfers/results 5-15 1e0 x10 en may19/projection_resultsen 5-15 1e0 2.csv")
-p3 <- read_csv("C:/Users/znhua/Documents/raspberry transfers/results 5-15 1e0 x10 en may19/projection_resultsen 5-15 1e0 3.csv")
-p4 <- read_csv("C:/Users/znhua/Documents/raspberry transfers/results 5-15 1e0 x10 en may19/projection_resultsen 5-15 1e0 4.csv")
-p5 <- read_csv("C:/Users/znhua/Documents/raspberry transfers/results 5-15 1e0 x10 en may19/projection_resultsen 5-15 1e0 5.csv")
+p2 <- read_csv("C:/Users/znhua/Documents/raspberry transfers/results 5-15 1e0 x10en may19/projection_resultsen 5-15 1e0 2.csv")
+p3 <- read_csv("C:/Users/znhua/Documents/raspberry transfers/results 5-15 1e0 x10en may19/projection_resultsen 5-15 1e0 3.csv")
+p4 <- read_csv("C:/Users/znhua/Documents/raspberry transfers/results 5-15 1e0 x10en may19/projection_resultsen 5-15 1e0 4.csv")
+p5 <- read_csv("C:/Users/znhua/Documents/raspberry transfers/results 5-15 1e0 x10en may19/projection_resultsen 5-15 1e0 5.csv")
 p2$itr <- ifelse(p2$itr == 0, 2, 3)
 p3$itr <- ifelse(p3$itr == 0, 4, 5)
 p4$itr <- ifelse(p4$itr == 0, 6, 7)
 p5$itr <- ifelse(p5$itr == 0, 8, 9)
 
 projection_results <- rbind(
-  read_csv("C:/Users/znhua/Documents/raspberry transfers/results 5-15 1e0 x10 en may19/projection_resultsen 5-15 1e0 1.csv"),
+  read_csv("C:/Users/znhua/Documents/raspberry transfers/results 5-15 1e0 x10en may19/projection_resultsen 5-15 1e0 1.csv"),
   p2, p3, p4, p5
 )
 
-read_csv("C:/Users/znhua/Documents/raspberry transfers/results 5-15 1e0 x10 en may19/verbreps_resultsen 5-15 1e0 1.csv")
+#read_csv("C:/Users/znhua/Documents/raspberry transfers/results 5-15 1e0 x10 en may19/verbreps_resultsen 5-15 1e0 1.csv")
 verbreps_results <- read_csv("C:/Users/znhua/Documents/raspberry transfers/verbreps_resultsen 5e0.csv")
 projection_results <- read_csv("C:/Users/znhua/Documents/raspberry transfers/projection_resultsen 5e0.csv")
 
 gleason_data <- read_csv("data/gleason_data.csv")
+# With penalty
 verbreps_results <- verbreps_results %>% filter(verb != "IMPERATIVE" & verb!="DECLARATIVE")
 # Without penalty
 verbreps_results <- verbreps_nopen_results  %>% filter(verb != "IMPERATIVE" & verb!="DECLARATIVE")
@@ -91,7 +92,7 @@ verbreps_results <- verbreps_kl_results  %>% filter(verb != "IMPERATIVE" & verb!
 
 ## END LOADING ENGLISH
 
-## Mandarin
+## START LOADING MANDARIN
 suff = "wpeno" # "wnopen" "wnopen"
 verbreps_results <- rbind(
   read_csv(paste("results/feb2020 draft/verbreps_resultsmc 22", suff, ".csv", sep = "")),
@@ -107,14 +108,14 @@ projection_results <- rbind(
 ## With penalty
 
 # v2 <- read_csv(paste("results/verbreps_resultsmc 1-2.csv", sep = "")) # no V2, as v1 has two itrs
-v3_mc <- read_csv(paste("results/verbreps_resultsmc 1-3.csv", sep = ""))
-v4_mc <- read_csv(paste("results/verbreps_resultsmc 1-4.csv", sep = ""))
-v5_mc <- read_csv(paste("results/verbreps_resultsmc 1-5.csv", sep = ""))
-v6_mc <- read_csv(paste("results/verbreps_resultsmc 1-6.csv", sep = ""))
-v7_mc <- read_csv(paste("results/verbreps_resultsmc 1-7.csv", sep = ""))
-v8_mc <- read_csv(paste("results/verbreps_resultsmc 1-8.csv", sep = ""))
-v9_mc <- read_csv(paste("results/verbreps_resultsmc 1-9.csv", sep = ""))
-v0_mc <- read_csv(paste("results/verbreps_resultsmc 1-0.csv", sep = ""))
+v3_mc <- read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 1e0 x10mc may23/verbreps_resultsmc 1-3.csv", sep = ""))
+v4_mc <- read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 1e0 x10mc may23/verbreps_resultsmc 1-4.csv", sep = ""))
+v5_mc <- read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 1e0 x10mc may23/verbreps_resultsmc 1-5.csv", sep = ""))
+v6_mc <- read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 1e0 x10mc may23/verbreps_resultsmc 1-6.csv", sep = ""))
+v7_mc <- read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 1e0 x10mc may23/verbreps_resultsmc 1-7.csv", sep = ""))
+v8_mc <- read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 1e0 x10mc may23/verbreps_resultsmc 1-8.csv", sep = ""))
+v9_mc <- read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 1e0 x10mc may23/verbreps_resultsmc 1-9.csv", sep = ""))
+v0_mc <- read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 1e0 x10mc may23/verbreps_resultsmc 1-0.csv", sep = ""))
 
 #v2$itr <- 2
 v3_mc$itr <- 3
@@ -126,18 +127,18 @@ v8_mc$itr <- 8
 v9_mc$itr <- 9
 v0_mc$itr <- 2
 verbreps_results <- rbind(
-  read_csv(paste("results/verbreps_resultsmc 1-1.csv", sep = "")),
+  read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 1e0 x10mc may23/verbreps_resultsmc 1-1.csv", sep = "")),
   v3_mc, v4_mc, v5_mc, v6_mc, v7_mc, v8_mc, v9_mc, v0_mc
 )
 
-p3_mc <- read_csv(paste("results/projection_resultsmc 1-3.csv", sep = ""))
-p4_mc <- read_csv(paste("results/projection_resultsmc 1-4.csv", sep = ""))
-p5_mc <- read_csv(paste("results/projection_resultsmc 1-5.csv", sep = ""))
-p6_mc <- read_csv(paste("results/projection_resultsmc 1-6.csv", sep = ""))
-p7_mc <- read_csv(paste("results/projection_resultsmc 1-7.csv", sep = ""))
-p8_mc <- read_csv(paste("results/projection_resultsmc 1-8.csv", sep = ""))
-p9_mc <- read_csv(paste("results/projection_resultsmc 1-9.csv", sep = ""))
-p0_mc <- read_csv(paste("results/projection_resultsmc 1-0.csv", sep = ""))
+p3_mc <- read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 1e0 x10mc may23/projection_resultsmc 1-3.csv", sep = ""))
+p4_mc <- read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 1e0 x10mc may23/projection_resultsmc 1-4.csv", sep = ""))
+p5_mc <- read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 1e0 x10mc may23/projection_resultsmc 1-5.csv", sep = ""))
+p6_mc <- read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 1e0 x10mc may23/projection_resultsmc 1-6.csv", sep = ""))
+p7_mc <- read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 1e0 x10mc may23/projection_resultsmc 1-7.csv", sep = ""))
+p8_mc <- read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 1e0 x10mc may23/projection_resultsmc 1-8.csv", sep = ""))
+p9_mc <- read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 1e0 x10mc may23/projection_resultsmc 1-9.csv", sep = ""))
+p0_mc <- read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 1e0 x10mc may23/projection_resultsmc 1-0.csv", sep = ""))
 
 #v2$itr <- 2
 p3_mc$itr <- 3
@@ -149,21 +150,21 @@ p8_mc$itr <- 8
 p9_mc$itr <- 9
 p0_mc$itr <- 2
 projection_results <- rbind(
-  read_csv(paste("results/projection_resultsmc 1-1.csv", sep = "")),
+  read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 1e0 x10mc may23/projection_resultsmc 1-1.csv", sep = "")),
   p3_mc, p4_mc, p5_mc, p6_mc, p7_mc, p8_mc, p9_mc, p0_mc
 )
 unique(projection_results$itr)
 
-## Without penalty (original)
-v2_mc_nopen <- read_csv(paste("results/verbreps_resultsmc 0-2.csv", sep = "")) # no V2, as v1 has two itrs
-v3_mc_nopen <- read_csv(paste("results/verbreps_resultsmc 0-3.csv", sep = ""))
-v4_mc_nopen <- read_csv(paste("results/verbreps_resultsmc 0-4.csv", sep = ""))
-v5_mc_nopen <- read_csv(paste("results/verbreps_resultsmc 0-5.csv", sep = ""))
-v6_mc_nopen <- read_csv(paste("results/verbreps_resultsmc 0-6.csv", sep = ""))
-v7_mc_nopen <- read_csv(paste("results/verbreps_resultsmc 0-7.csv", sep = ""))
-v8_mc_nopen <- read_csv(paste("results/verbreps_resultsmc 0-8.csv", sep = ""))
-v9_mc_nopen <- read_csv(paste("results/verbreps_resultsmc 0-9.csv", sep = ""))
-v0_mc_nopen <- read_csv(paste("results/verbreps_resultsmc 0-0.csv", sep = ""))
+## Without penalty (original model)
+v2_mc_nopen <- read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 0e0 x10mc may23/verbreps_resultsmc 0-2.csv", sep = "")) # no V2, as v1 has two itrs
+v3_mc_nopen <- read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 0e0 x10mc may23/verbreps_resultsmc 0-3.csv", sep = ""))
+v4_mc_nopen <- read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 0e0 x10mc may23/verbreps_resultsmc 0-4.csv", sep = ""))
+v5_mc_nopen <- read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 0e0 x10mc may23/verbreps_resultsmc 0-5.csv", sep = ""))
+v6_mc_nopen <- read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 0e0 x10mc may23/verbreps_resultsmc 0-6.csv", sep = ""))
+v7_mc_nopen <- read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 0e0 x10mc may23/verbreps_resultsmc 0-7.csv", sep = ""))
+v8_mc_nopen <- read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 0e0 x10mc may23/verbreps_resultsmc 0-8.csv", sep = ""))
+v9_mc_nopen <- read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 0e0 x10mc may23/verbreps_resultsmc 0-9.csv", sep = ""))
+v0_mc_nopen <- read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 0e0 x10mc may23/verbreps_resultsmc 0-0.csv", sep = ""))
 
 v2_mc_nopen$itr <- 2
 v3_mc_nopen$itr <- 3
@@ -175,7 +176,7 @@ v8_mc_nopen$itr <- 8
 v9_mc_nopen$itr <- 9
 v0_mc_nopen$itr <- 1
 verbreps_mc_results <- rbind(
-  read_csv(paste("results/verbreps_resultsmc 0-1.csv", sep = "")),
+  read_csv(paste("C:/Users/znhua/Documents/raspberry transfers/results 0e0 x10mc may23/verbreps_resultsmc 0-1.csv", sep = "")),
   v2_mc_nopen, v3_mc_nopen, v4_mc_nopen, v5_mc_nopen, v6_mc_nopen, v7_mc_nopen, v8_mc_nopen, v9_mc_nopen, v0_mc_nopen
 )
 unique(verbreps_mc_results$itr)
@@ -211,18 +212,19 @@ repmeans.summ$verbPlot <- recode(repmeans.summ$verb,
                                  DECLARATIVE = "DECLARATIVE",
                                  IMPERATIVE = "IMPERATIVE",
                                  "要" = "yao 'want' (D)",
-                                 "看" = "kan 'see' (B)", 
                                  "说" = "shuo 'say' (B)",
-                                 "看看" = "kankan 'see-DUP' (B)", 
-                                 "讲" = "jiang 'say' (B)",
+                                 "看" = "kan 'see' (B)", 
                                  "想" = "xiang 'think, want' (B, D)", 
-                                 "知道" = "zhidao 'know' (B)", 
-                                 "叫" = "jiao 'call/get' (D)",
-                                 "喜欢" = "xihuan 'like' (D)",  
-                                 "告诉" = "gaosu 'tell' (B)", 
                                  "帮" = "bang 'help' (O)", 
                                  "让" = "rang 'let' (O)", 
+                                 "喜欢" = "xihuan 'like' (D)",  
+                                 "看看" = "kankan 'see-DUP' (B)", 
                                  "觉得" = "juede 'feel' (B)",
+                                 "讲" = "jiang 'say' (B)",
+                                 "知道" = "zhidao 'know' (B)", 
+                                 "告诉" = "gaosu 'tell' (B)", 
+                                 "叫" = "jiao 'call/get' (D)",
+                                 
                                  "want" = "want (D)", 
                                  "see" = "see (B)", 
                                  "know" = "know (B)", 
@@ -238,6 +240,7 @@ repmeans.summ$verbPlot <- recode(repmeans.summ$verb,
 repmeans.summ <- repmeans.summ %>% filter(verbPlot != "<NA>")
 
 #tikz('~/experiments/MainClauseModel/bin/verbrep_prob.tikz', width=5.5, height=4)
+#Try 800x400 px
 ggplot(repmeans.summ
        %>% filter(verbPlot != "vacuous-verb"
          & verbPlot != "IMPERATIVE" & verbPlot !="DECLARATIVE"
@@ -279,18 +282,18 @@ repmeans.emb$verb <- recode(repmeans.emb$verb,
                                  DECLARATIVE = "DECLARATIVE",
                                  IMPERATIVE = "IMPERATIVE",
                                  "要" = "yao\n'want' (D)",
-                                 "看" = "kan\n'see' (B)", 
                                  "说" = "shuo\n'say' (B)",
-                                 "看看" = "kankan\n'see-DUP' (B)", 
-                                 "讲" = "jiang\n'say' (B)",
+                                 "看" = "kan\n'see' (B)", 
                                  "想" = "xiang\n'think, want' (B, D)", 
-                                 "知道" = "zhidao\n'know' (B)", 
-                                 "叫" = "jiao\n'call/get' (D)",
-                                 "喜欢" = "xihuan\n'like' (D)",  
-                                 "告诉" = "gaosu\n'tell' (B)", 
-                                 "帮" = "bang\n'help' (O)", 
                                  "让" = "rang\n'let' (O)", 
+                                 "喜欢" = "xihuan\n'like' (D)",  
                                  "觉得" = "juede\n'feel' (B)",
+                                 "讲" = "jiang\n'say' (B)",
+                                 "知道" = "zhidao\n'know' (B)", 
+                                 "告诉" = "gaosu\n'tell' (B)", 
+                                 "叫" = "jiao\n'call/get' (D)",
+                                 "帮" = "bang\n'help' (O)", 
+                            
                                  "want" = "want (D)", 
                                  "see" = "see (B)", 
                                  "know" = "know (B)", 
@@ -306,18 +309,17 @@ repmeans.emb.cast$verb <- recode(repmeans.emb.cast$verb,
                                 DECLARATIVE = "DECLARATIVE",
                                 IMPERATIVE = "IMPERATIVE",
                                 "要" = "yao\n'want' (D)",
-                                "看" = "kan\n'see' (B)", 
                                 "说" = "shuo\n'say' (B)",
-                                "看看" = "kankan\n'see-DUP' (B)", 
-                                "讲" = "jiang\n'say' (B)",
+                                "看" = "kan\n'see' (B)", 
                                 "想" = "xiang\n'think, want' (B, D)", 
-                                "知道" = "zhidao\n'know' (B)", 
-                                "叫" = "jiao\n'call/get' (D)",
-                                "喜欢" = "xihuan\n'like' (D)",  
-                                "告诉" = "gaosu\n'tell' (B)", 
-                                "帮" = "bang\n'help' (O)", 
                                 "让" = "rang\n'let' (O)", 
+                                "喜欢" = "xihuan\n'like' (D)",  
                                 "觉得" = "juede\n'feel' (B)",
+                                "讲" = "jiang\n'say' (B)",
+                                "知道" = "zhidao\n'know' (B)", 
+                                "告诉" = "gaosu\n'tell' (B)", 
+                                "叫" = "jiao\n'call/get' (D)",
+                                "帮" = "bang\n'help' (O)", 
                                 "want" = "want (D)", 
                                 "see" = "see (B)", 
                                 "know" = "know (B)", 
